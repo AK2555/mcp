b8c7f51c289d058e47cf6e186db3fd657f1b24cd
Ayush Kumar <ayushkumar@Ayushs-MacBook-Air.local>
2026-08-20 01:45:46 +0530

    MCP crash course

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import asyncio
 from dotenv import load_dotenv
+from langchain_core.tools import tool
 from langchain_openai import ChatOpenAI
 from mcp import ClientSession, StdioServerParameters
 from mcp.client.stdio import stdio_client
+from langchain_core.messages import HumanMessage
 from langchain_mcp_adapters.tools import load_mcp_tools
+from langchain.agents import create_agent
 
 load_dotenv()
 
@@ -17,6 +20,16 @@ stdio_server_params = StdioServerParameters(
 
 async def main():
     print("Hello from mcp-crash-course!")
+    async with stdio_client(stdio_server_params) as (read,write):
+        async with ClientSession(read_stream=read, write_stream=write) as session:
+            await session.initialize()
+            print("session initialized")
+            tools = await load_mcp_tools(session)
+            
+            agent = create_agent(llm, tools)
+            
+            result = await agent.ainvoke({"messages": [HumanMessage(content="What is 2 + 2?")]})
+            print(result["messages"][-1].content)
 
 
 if __name__ == "__main__":
