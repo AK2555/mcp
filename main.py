49ccad3752e116409858b13ff8436363e369acaa
Ayush Kumar <ayushkumar@Ayushs-MacBook-Air.local>
2026-08-20 01:16:01 +0530

    mcp client

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 import asyncio
 from dotenv import load_dotenv
-import os
+from langchain_openai import ChatOpenAI
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
+from langchain_mcp_adapters.tools import load_mcp_tools
 
 load_dotenv()
-print(os.getenv("OPENAI_API_KEY"))
+
+llm = ChatOpenAI()
+
+stdio_server_params = StdioServerParameters(
+    command="python",
+    args=["/Users/ayushkumar/mcp/mcp-crash-course/servers/math_server.py"],
+)
+
 
 async def main():
     print("Hello from mcp-crash-course!")
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+     asyncio.run(main())
